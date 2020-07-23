922163d1d8e88580181c30135a8b4fede5028cfb
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-23 23:46:12 +0530

    adding colors commiting

--- db.py
+++ db.py
@@ -6,16 +6,18 @@ app.title('databse entry')
 
 
 
-firstname = Entry(app,bg='white').grid(row=0,column=1)
-lastname = Entry(app,bg='white').grid(row=2,column=1)
-age = Entry(app,bg='white').grid(row=3,column=1)
-city = Entry(app,bg='white').grid(row=4,column=1)
-pincode = Entry(app,bg='white').grid(row=5,column=1)
+firstname = Entry(app,bg='light pink').grid(row=0,column=1)
+lastname = Entry(app,bg='light pink').grid(row=2,column=1)
+age = Entry(app,bg='light pink').grid(row=3,column=1)
+city = Entry(app,bg='light pink').grid(row=4,column=1)
+pincode = Entry(app,bg='light pink').grid(row=5,column=1)
 
 firstname1 = Label(app,bg='white',text ='firstname').grid(row=0,column=0)
 lastname1 = Label(app,bg='white',text ='lastname').grid(row=2,column=0)
 age1 = Label(app,bg='white',text ='age').grid(row=3,column=0)
 city1 = Label(app,bg='white',text ='city').grid(row=4,column=0)
 pincode1 = Label(app,bg='white',text ='pincode').grid(row=5,column=0)
+
+
 
 app.mainloop()
