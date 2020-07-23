84a046b113fa3835621bfd5b7ef313a9682ec79d
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-23 23:49:24 +0530

    adding colors commiting

--- db.py
+++ db.py
@@ -12,11 +12,11 @@ age = Entry(app,bg='light pink').grid(row=3,column=1)
 city = Entry(app,bg='light pink').grid(row=4,column=1)
 pincode = Entry(app,bg='light pink').grid(row=5,column=1)
 
-firstname1 = Label(app,bg='white',text ='firstname').grid(row=0,column=0)
-lastname1 = Label(app,bg='white',text ='lastname').grid(row=2,column=0)
-age1 = Label(app,bg='white',text ='age').grid(row=3,column=0)
-city1 = Label(app,bg='white',text ='city').grid(row=4,column=0)
-pincode1 = Label(app,bg='white',text ='pincode').grid(row=5,column=0)
+firstname1 = Label(app,bg='white',text ='firstname',fg = 'blue').grid(row=0,column=0)
+lastname1 = Label(app,bg='white',text ='lastname',fg = 'blue').grid(row=2,column=0)
+age1 = Label(app,bg='white',text ='age',fg = 'blue').grid(row=3,column=0)
+city1 = Label(app,bg='white',text ='city',fg = 'blue').grid(row=4,column=0)
+pincode1 = Label(app,bg='white',text ='pincode',fg = 'blue').grid(row=5,column=0)
 
 
 
