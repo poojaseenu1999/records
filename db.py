dccbf99d8990ba298dfd2a0531eadf33e792be84
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-23 23:33:20 +0530

    adding fields commiting

--- db.py
+++ db.py
@@ -6,5 +6,10 @@ app.title('databse entry')
 
 
 
-name = Entry(app,bg='white').grid(row=0,column=1)
+firstname = Entry(app,bg='white').grid(row=0,column=1)
+lastname = Entry(app,bg='white').grid(row=2,column=1)
+age = Entry(app,bg='white').grid(row=3,column=1)
+city = Entry(app,bg='white').grid(row=4,column=1)
+pincode = Entry(app,bg='white').grid(row=5,column=1)
+
 app.mainloop()
