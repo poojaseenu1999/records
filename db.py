7811c10af2a37162b1256293d04e5ad87f781aea
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-25 22:34:34 +0530

    adding database

--- db.py
+++ db.py
@@ -1,35 +1,29 @@
 from tkinter import *
 import sqlite3
 
-# conn=sqlite3.connect("python.db")
-# cur=conn.cursor()
-# cur.execute("CREATE TABLE  database (firstname TEXT,lastname TEXT,address TEXT,coty TEXT,state TEXT,pincode NUMBER) ")
-# conn.commit()
-# conn.close()
-
-def submit():
-    conn=sqlite3.connect("python.db")
-    cur=conn.cursor()
-    cur.execute("INSERT INTO database VALUES(:f_name, :l_name, :address, :city, :state, :pincode)")
-    {
-      'f_name':f_name.get(),
-      'l_name':l_name.get(),
-      'address':address.get(),
-      'city':city.get(),
-      'pincode':pincode.get()
-     }
-    conn.commit()
-    conn.close()
-
-
 app = Tk()
 app.title('databse entry')
 
 
+# conn=sqlite3.connect("python1.db")
+# cur=conn.cursor()
+# cur.execute("CREATE TABLE  student (firstname TEXT,lastname TEXT,age integer,city TEXT,pincode NUMBER) ")
 
 
-firstname = Entry(app,bg='light pink').grid(row=0,column=1)
-lastname = Entry(app,bg='light pink').grid(row=2,column=1)
+def submit():
+
+    f_name.delete(0,END)
+    l_name.delete(0,END)
+    age.delete(0,END)
+    city.delete(0,END)
+    pincode.delete(0,END)
+
+
+
+
+
+f_name = Entry(app,bg='light pink').grid(row=0,column=1)
+l_name = Entry(app,bg='light pink').grid(row=2,column=1)
 age = Entry(app,bg='light pink').grid(row=3,column=1)
 city = Entry(app,bg='light pink').grid(row=4,column=1)
 pincode = Entry(app,bg='light pink').grid(row=5,column=1)
@@ -40,8 +34,10 @@ age1 = Label(app,bg='white',text ='age',fg = 'blue').grid(row=3,column=0)
 city1 = Label(app,bg='white',text ='city',fg = 'blue').grid(row=4,column=0)
 pincode1 = Label(app,bg='white',text ='pincode',fg = 'blue').grid(row=5,column=0)
 
-but1=Button(app,text='add entry to database').grid(row=6,column=1)
+but1=Button(app,text='add entry to database',command=submit).grid(row=6,column=1)
 but2=Button(app,text='list the entry').grid(row=7,column=1)
 
+# conn.commit()
+# conn.close()
 
 app.mainloop()
